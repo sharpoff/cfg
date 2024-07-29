e1e29e81a09396c51729067f4139b364e89960db
Sharp <sharpyeah@gmail.com>
2024-07-29 16:04:25 +0900

    some changes

--- .bash_profile
+++ .bash_profile
@@ -2,4 +2,4 @@
 [[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx
 
 . "$HOME/.cargo/env"
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/include/freetype2:$LD_LIBRARY_PATH
--- .config/suckless/st/config.def.h
+++ .config/suckless/st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Consolas:pixelsize=18:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font:pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
