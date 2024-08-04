3ff63e7b6b18372ba11bd15895ade1191d66bf2e
Sharp <sharpyeah@gmail.com>
2024-08-04 19:31:00 +0900

    nvim chagnes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,8 +4,6 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 Plug('morhetz/gruvbox')
 Plug('neovim/nvim-lspconfig')
-Plug('hrsh7th/nvim-cmp')
-Plug('hrsh7th/cmp-nvim-lsp')
 vim.call('plug#end')
 
 require("settings")
--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,7 +1,5 @@
 local opts = { noremap=true, silent=true }
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
 vim.opt.completeopt = { "menu" }
 
 local on_attach = function(client, bufnr)
@@ -13,7 +11,6 @@ end
 local servers = {'lua_ls', 'clangd'}
 for _, lsp in ipairs(servers) do
     require('lspconfig')[lsp].setup {
-        capabilities = capabilities,
         on_attach = on_attach,
     }
 end
@@ -23,27 +20,3 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
     border = "rounded",
   }
 )
-
--- cmp setup
-
-local cmp = require'cmp'
-cmp.setup({
-    snippet = {
-        -- REQUIRED - you must specify a snippet engine
-        expand = function(args)
-            vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-        end,
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
-    sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-    }, {
-        { name = 'buffer' },
-    }),
-})
