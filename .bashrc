ef885da1a38f584c0bca019fa8cd52295898e5d6
sharpoff <sharpyeah@gmail.com>
2024-10-14 00:07:42 +0900

    fix lsp and add rust compiler to neovim

--- .bashrc
+++ .bashrc
@@ -7,4 +7,4 @@ alias grep='grep --color=auto'
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME' # alias for git dotfiles
 export PS1="\u@\h:\[\e[32m\]\w\[\e[0m\]\$ "
 export EDITOR='vim'
-export PATH="$PATH:usr/local/bin:$HOME/.cabal/bin:$HOME/.ghcup/bin:$(go env GOBIN):$(go env GOPATH)/bin"
+export PATH="$PATH:usr/local/bin:$HOME/.cabal/bin:$HOME/.ghcup/bin:$(go env GOBIN):$(go env GOPATH)/bin:~/.cargo/bin"
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,17 +1,17 @@
 local lspconfig = require('lspconfig')
 vim.opt.completeopt = { "menu" }
 
-local function on_attach()
-    vim.keymap.set("n", "K", vim.lsp.buf.hover())
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition())
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration())
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action(), opts)
-    vim.keymap.set("n", "<leader>rf", vim.lsp.buf.references())
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename())
-    vim.keymap.set("n", "<leader>:", vim.diagnostic.open_float())
-    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help())
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_next())
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_prev())
+local on_attach = function(client, bufnr)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+    vim.keymap.set("n", "<leader>rf", vim.lsp.buf.references)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+    vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
+    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help)
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_next)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_prev)
 end
 
 lspconfig.lua_ls.setup{}
@@ -22,6 +22,16 @@ lspconfig.clangd.setup {
         "--header-insertion=never"
     }
 }
-lspconfig.rust_analyzer.setup{}
+require'lspconfig'.rust_analyzer.setup{
+    on_attach = on_attach,
+    settings = {
+        ['rust-analyzer'] = {
+            diagnostics = {
+                enable = false;
+            }
+        }
+    }
+}
+
 lspconfig.gopls.setup{}
 lspconfig.jdtls.setup{}
